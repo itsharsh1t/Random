20bf269074de6f573f890eacf2c4116f0239d0a3
Harshit <81919040+itsharsh1t@users.noreply.github.com>
2022-01-14 21:10:36 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,28 @@
+import random
+
+lower = input('Enter lower number:')
+
+upper = input('Enter upper number:')
+
+random_number = random.randint(lower,upper)
+
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
